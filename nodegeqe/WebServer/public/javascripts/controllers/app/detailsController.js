f893ab6e93da3f1020a8430d32e5b264d6f112e9
Justin <justin.lueders@soteradefense.com>
2015-04-20 19:21:51 -0500

    numbers wrap around when scrolling term highlighting is in and working

--- nodegeqe/WebServer/public/javascripts/controllers/app/detailsController.js
+++ nodegeqe/WebServer/public/javascripts/controllers/app/detailsController.js
@@ -3,6 +3,8 @@ angular.module('NodeWebBase')
         $scope.data = null;
         $scope.currentItemIndex = null;
         $scope.displayIndex = null;
+        $scope.displayCaptionHtml = "None";
+        $scope.termArray = [];
         $scope.currentItem = {"usr":"unknown",
                                 "cap":"none",
                                 "sco":"0"};
@@ -16,16 +18,50 @@ angular.module('NodeWebBase')
             });
         });
 
+        $rootScope.$on("setTermDictionary", function(event,data){
+            $scope.$apply(function () {
+                    angular.forEach(data,function(entry,idx){
+                        $scope.termArray.push(entry[0]);
+                    })
+                });
+        });
+
+        $scope.highlightText = function(text){
+            var words = text.split(' ');
+            angular.forEach($scope.termArray,function(term,idx){
+                angular.forEach(words,function(word,idx){
+                    if(word.toLowerCase() == term.toLowerCase()){
+                        words[idx] = '<span class="highlight">' + word + "</span>";
+                    }
+                    else{
+                    }
+                });
+            });
+            return words.join(' ');
+        };
+
         $scope.next = function(){
             $scope.currentItemIndex++;
+
+            if($scope.currentItemIndex >= $scope.data.posts.length)
+                $scope.currentItemIndex = 0;
+
             $scope.displayIndex = $scope.currentItemIndex+1;
             $scope.currentItem = $scope.data.posts[$scope.currentItemIndex];
+            $scope.displayCaptionHtml = $scope.highlightText($scope.currentItem.cap);
+            $("#caption").html($scope.displayCaptionHtml);
         };
 
         $scope.previous = function(){
             $scope.currentItemIndex--;
+
+            if($scope.currentItemIndex < 0)
+                $scope.currentItemIndex = $scope.data.posts.length-1;
+
             $scope.displayIndex = $scope.currentItemIndex+1;
             $scope.currentItem = $scope.data.posts[$scope.currentItemIndex];
+            $scope.displayCaptionHtml = $scope.highlightText($scope.currentItem.cap);
+            $("#caption").html($scope.displayCaptionHtml);
         };
 
         $scope.getUser = function(){
