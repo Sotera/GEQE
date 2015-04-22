eaf3f03429fdb25165e2ec9a837343a58dad4efc
Justin <justin.lueders@soteradefense.com>
2015-04-22 14:14:12 -0500

    check bin by lat lon by default

--- nodegeqe/WebServer/routes/app/controlBox.js
+++ nodegeqe/WebServer/routes/app/controlBox.js
@@ -7,12 +7,21 @@ router.get('/:vp', function (req, res) {
     var routeName = req.params.vp;
     netHelpers.performAjaxRequest('localhost', 8080, '/' + routeName, 'GET', req.query, function (resultObject) {
         if (resultObject.error) {
+            if(!resultObject || !resultObject.error || !resultObject.error.message){
+                res.status(501).send("Error processing request.");
+            }
             res.status(resultObject.error.status).send(resultObject.error.message);
             return;
         }
 
         res.status(200).send(resultObject);
+
     },function (error) {
+            if(!error.message) {
+                console.log(error);
+                res.status(500).send(error);
+                return;
+            }
             console.log(error.message);
             res.status(500).send(error.message);
     })
