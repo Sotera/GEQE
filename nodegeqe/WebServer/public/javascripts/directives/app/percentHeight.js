ab8647c51d7a8ff9f66922251207042afab048d6
Justin <justin.lueders@soteradefense.com>
2015-04-29 10:54:39 -0500

    lots of css and formatting

--- nodegeqe/WebServer/public/javascripts/directives/app/percentHeight.js
+++ nodegeqe/WebServer/public/javascripts/directives/app/percentHeight.js
@@ -2,21 +2,31 @@ angular.module('NodeWebBase')
     .directive('percentHeight',function () {
         function link(scope, element, attrs) {
             $(document).ready(function () {
-                var remainingHeight = $(window).height()-75;
+                var remainingHeight = $(window).height()-48;
                 var percent = parseInt(attrs.percent)/100;
                 scope.style = {
                     height: (remainingHeight * percent).toString() + 'px'
                 };
 
             });
+            scope.delay = (function(){
+                var timer = 0;
+                return function(callback, ms){
+                    clearTimeout (timer);
+                    timer = setTimeout(callback, ms);
+                };
+            })();
+
             $(window).resize(function () {
-                var remainingHeight = $(window).height()-75;
-                var percent = parseInt(attrs.percent)/100;
-                scope.$apply(function(){
-                    scope.style = {
-                        height: (remainingHeight * percent).toString() + 'px'
-                    };
-                });
+                scope.delay(function(){
+                    var remainingHeight = $(window).height()-48;
+                    var percent = parseInt(attrs.percent)/100;
+                    scope.$apply(function(){
+                        scope.style = {
+                            height: (remainingHeight * percent).toString() + 'px'
+                        };
+                    });
+                }, 100);
             });
 
         }
