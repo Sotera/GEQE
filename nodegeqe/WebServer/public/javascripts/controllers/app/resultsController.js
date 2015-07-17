a220b5552e77829cee9e486da307782407971c80
Justin <justin.lueders@soteradefense.com>
2015-07-17 14:40:10 -0500

    update dict to show per cluster data

--- nodegeqe/WebServer/public/javascripts/controllers/app/resultsController.js
+++ nodegeqe/WebServer/public/javascripts/controllers/app/resultsController.js
@@ -1,10 +1,37 @@
 angular.module('NodeWebBase')
-    .controller('resultsController',['$scope','$sce','$rootScope',function ($scope, $sce, $rootScope) {
+    .controller('resultsController',['$scope','$sce','$rootScope','$timeout',function ($scope, $sce, $rootScope,$timeout) {
+        $scope.masterCollection = [];
+        $scope.rowCollection = [];
+        $scope.displayedCollection = [];
 
-        $rootScope.$on('displayResults', function (event, data) {
-            $("#resultsText").html(data);
-        });
+
         $rootScope.$on('clearResults', function (event) {
-            $("#resultsText").html("");
+            $scope.clearResults();
         });
+
+        $rootScope.$on('loadItemData', function (event, data) {
+
+            $scope.clearResults();
+
+            $timeout(function(){
+                if(data.dict) {
+                    var dictData = [];
+                    angular.forEach(data.dict, function(term,idx){
+                        dictData.push({"term":term,"rank":idx});
+
+                    });
+
+                    $scope.masterCollection = dictData.slice(0);
+                    $scope.rowCollection = dictData.slice(0);
+                    $scope.displayedCollection = [].concat($scope.rowCollection);
+                }
+            });
+
+        });
+
+        $scope.clearResults = function(){
+            $scope.masterCollection.length = 0;
+            $scope.rowCollection.length = 0;
+            $scope.displayedCollection.length = 0;
+        };
     }]);
