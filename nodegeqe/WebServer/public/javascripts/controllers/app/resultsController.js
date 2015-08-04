a4df9daeacd4d2fa4520ca2cbe54e2595e6ff965
Justin <justin.lueders@soteradefense.com>
2015-08-04 16:19:51 -0500

    word cloud

--- nodegeqe/WebServer/public/javascripts/controllers/app/resultsController.js
+++ nodegeqe/WebServer/public/javascripts/controllers/app/resultsController.js
@@ -4,13 +4,6 @@ angular.module('NodeWebBase')
         $scope.rowCollection = [];
         $scope.displayedCollection = [];
         $scope.words= [
-            {text: "Lorem", weight: 13},
-            {text: "Ipsum", weight: 10.5},
-            {text: "Dolor", weight: 9.4},
-            {text: "Sit", weight: 8},
-            {text: "Amet", weight: 6.2},
-            {text: "Consectetur", weight: 5},
-            {text: "Adipiscing", weight: 5}
         ];
 
         $rootScope.$on('clearResults', function (event) {
