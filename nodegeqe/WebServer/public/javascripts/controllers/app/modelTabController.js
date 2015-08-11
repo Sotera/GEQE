0862ae6bb261a74c9c4fd243bc0e707df9dff968
Marek Bejda <marek.bejda@soteradefense.com>
2015-08-11 11:44:30 -0500

    halfway done with merge...

--- nodegeqe/WebServer/public/javascripts/controllers/app/modelTabController.js
+++ nodegeqe/WebServer/public/javascripts/controllers/app/modelTabController.js
@@ -2,12 +2,12 @@
  * Created by jlueders on 4/13/15.
  */
 angular.module('NodeWebBase')
-    .controller('modelTabController', ['$scope','$rootScope','$http','setSelectionMsg', function ($scope, $rootScope, $http,setSelectionMsg) {
+    .controller('modelTabController', ['$scope', '$rootScope', '$http', 'setSelectionMsg', function ($scope, $rootScope, $http, setSelectionMsg) {
         $scope.scoreFiles = [];
         $scope.polygonFiles = [];
 
 
-        $scope.popScore = function() {
+        $scope.popScore = function () {
             if (!$rootScope.isAppConfigured())
                 return;
 
@@ -23,13 +23,13 @@ angular.module('NodeWebBase')
         };
 
 
-        $scope.populatePolygonSelect = function() {
-            if(!$rootScope.isAppConfigured())
+        $scope.populatePolygonSelect = function () {
+            if (!$rootScope.isAppConfigured())
                 return;
 
             $http({
-                method:"GET",
-                url: "app/sitelists/list/"+$rootScope.username,
+                method: "GET",
+                url: "app/sitelists/list/" + $rootScope.username,
                 params: {}
             }).success(function (response) {
                 $scope.polygonFiles = response
@@ -37,126 +37,130 @@ angular.module('NodeWebBase')
         };
 
 
-        $scope.drawPolygonFile = function(){
+        $scope.drawPolygonFile = function () {
             var modelId = $scope.getPolygonId();
-            if (!modelId)  $rootScope.showErrorMessage("Polygon name invalid.",'Polygon must be saved prior to use.');
+            if (!modelId)  $rootScope.showErrorMessage("Polygon name invalid.", 'Polygon must be saved prior to use.');
             else $rootScope.$emit("drawPolygonFile", modelId)
 
-        setSelectionMsg.listen(function(evt,vals){
-            if(vals.type="polygonSetSelected")
-                $scope.polygonSetSelected=vals.data;
-            if(vals.type="dataSetSelected")
-                $scope.dataSetSelected=vals.data;
-            console.log(vals);
-        });
+            setSelectionMsg.listen(function (evt, vals) {
+                if (vals.type = "polygonSetSelected")
+                    $scope.polygonSetSelected = vals.data;
+                if (vals.type = "dataSetSelected")
+                    $scope.dataSetSelected = vals.data;
+                console.log(vals);
+            });
+        }
+
+            $scope.getScoresModel = {
+                user: "",
+                fileAppOut: "",
+                maxOut: 50
+            };
 
 
-
-        $scope.getScoresModel = {
-            user: "",
-            fileAppOut: "",
-            maxOut:50
-        };
-
-
-        /**
-         * Get the id for the current polyFile from the polygonList
-         * Undefined if the model has not been saved
-         */
-        $scope.getPolygonId = function(){
-            for  (var i=0; i<$scope.polygonFiles.length; i++){
-                if ($scope.polygonFiles[i].name == $scope.polyFile){
-                    return $scope.polygonFiles[i].id;
-                }
-            }
-            return undefined;
-        };
-
-
-        /**
-         * Save the site list (polygon)
-         */
-        $scope.saveList = function(){
-
-            $rootScope.$emit("getShapesText",
-                {
-                    "scope":this,
-                    "callback":function(resultsText){
-                        if(!$rootScope.isAppConfigured())
-                            return;
-                        var pName = $scope.polyFile;
-                        var siteList = JSON.parse(resultsText);
-                        siteList.name = pName;
-                        siteList.username = $rootScope.username;
-                        var modelId = $scope.getPolygonId();
-                        if (modelId) siteList.id = modelId;
-
-                        $http({
-                            method:"POST",
-                            url: "app/sitelists/save",
-                            params: {
-                                siteList: siteList
-                            }}).success(function (response) {
-                            $("#resultsText").text(pName + " written");
-                            $scope.populatePolygonSelect(); // refresh the polygon list to get the new id
-                        }).error($rootScope.showError)
+            /**
+             * Get the id for the current polyFile from the polygonList
+             * Undefined if the model has not been saved
+             */
+            $scope.getPolygonId = function () {
+                for (var i = 0; i < $scope.polygonFiles.length; i++) {
+                    if ($scope.polygonFiles[i].name == $scope.polyFile) {
+                        return $scope.polygonFiles[i].id;
                     }
-                });
-        };
-
-        $rootScope.$on('sampleShape',function(event,item){
-
-            if(!item)
-                return;
-            $http({
-                method: "POST",
-                url: "app/posts/sitelist",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                data:{
-                    "sites": [item],
-                    "from":0,
-                    "size":100
                 }
-            }).success(function (response) {
-                $rootScope.$emit("clearMarkers",['training']);
-                $rootScope.$emit("drawMapMarkers",response.hits, "training");
-            }).error($rootScope.showError);
-        });
+                return undefined;
+            };
 
-        $scope.getDataSets = function() {
-            if (!$rootScope.isAppConfigured())
-                return;
 
-            $http({
-                method:"GET",
-                url: "app/controlBox/populate/trainingdata",
-                params:{
-                    user: $rootScope.username
-                }}).success(function (response) {
-                        $scope.trainingFiles = response.lFiles;
-                 }).error($rootScope.showError)
-        };
+            /**
+             * Save the site list (polygon)
+             */
+            $scope.saveList = function () {
 
-        $scope.trainingDataModel = {
-            user: "",
-            fileAppOut:""
-        };
+                $rootScope.$emit("getShapesText",
+                    {
+                        "scope": this,
+                        "callback": function (resultsText) {
+                            if (!$rootScope.isAppConfigured())
+                                return;
+                            var pName = $scope.polyFile;
+                            var siteList = JSON.parse(resultsText);
+                            siteList.name = pName;
+                            siteList.username = $rootScope.username;
+                            var modelId = $scope.getPolygonId();
+                            if (modelId) siteList.id = modelId;
 
-        $scope.drawTrainingData = function() {
-            if(!$rootScope.isAppConfigured())
-                return;
-            $scope.trainingDataModel.user = $rootScope.username;
-            $http({
-                method:"GET",
-                url: "app/controlBox/getTrainingData",
-                params: $scope.trainingDataModel})
-                .success(function (response) {
-                    $rootScope.$emit("clearMarkers",['training']);
-                    $rootScope.$emit("drawMapMarkers",response.sco, "training");
+                            $http({
+                                method: "POST",
+                                url: "app/sitelists/save",
+                                params: {
+                                    siteList: siteList
+                                }
+                            }).success(function (response) {
+                                $("#resultsText").text(pName + " written");
+                                $scope.populatePolygonSelect(); // refresh the polygon list to get the new id
+                            }).error($rootScope.showError)
+                        }
+                    });
+            };
+
+            $rootScope.$on('sampleShape', function (event, item) {
+
+                if (!item)
+                    return;
+                $http({
+                    method: "POST",
+                    url: "app/posts/sitelist",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    data: {
+                        "sites": [item],
+                        "from": 0,
+                        "size": 100
+                    }
+                }).success(function (response) {
+                    $rootScope.$emit("clearMarkers", ['training']);
+                    $rootScope.$emit("drawMapMarkers", response.hits, "training");
+                }).error($rootScope.showError);
+            });
+
+            $scope.getDataSets = function () {
+                if (!$rootScope.isAppConfigured())
+                    return;
+
+                $http({
+                    method: "GET",
+                    url: "app/controlBox/populate/trainingdata",
+                    params: {
+                        user: $rootScope.username
+                    }
+                }).success(function (response) {
+                    $scope.trainingFiles = response.lFiles;
+                }).error($rootScope.showError)
+            };
+
+            $scope.trainingDataModel = {
+                user: "",
+                fileAppOut: ""
+            };
+
+            $scope.drawTrainingData = function () {
+                if (!$rootScope.isAppConfigured())
+                    return;
+                $scope.trainingDataModel.user = $rootScope.username;
+                $http({
+                    method: "GET",
+                    url: "app/controlBox/getTrainingData",
+                    params: $scope.trainingDataModel
                 })
-                .error($rootScope.showError)
-        };
-   }]);
+                    .success(function (response) {
+                        $rootScope.$emit("clearMarkers", ['training']);
+                        $rootScope.$emit("drawMapMarkers", response.sco, "training");
+                    })
+                    .error($rootScope.showError)
+            };
+        }
+        ])
+        ;
 
