806c1f9b2a468df37cbcbcbcb1a421966a75a1bb
Luke Wendling <luke@lukewendling.com>
2015-10-20 17:38:17 -0500

    user is logged in after creating account

--- WebServer/public/javascripts/controllers/signupController.js
+++ WebServer/public/javascripts/controllers/signupController.js
@@ -1,32 +1,35 @@
 angular.module('NodeWebBase')
-		.constant('signupUrl', '/login/signup')
-		.controller('signupController', ['$scope', '$http', '$window', 'signupUrl',"ngDialog",
-		function ($scope, $http, $window, signupUrl,ngDialog) {
-			$scope.data = {};
+  .constant('signupUrl', '/login/signup')
+  .controller('signupController', ['$scope', '$http', '$window', '$cookies', 'signupUrl', 'ngDialog', 'authService',
+    function ($scope, $http, $window, $cookies, signupUrl, ngDialog, authService) {
+      $scope.data = {};
 
-			$scope.cancelSignup=function(){
-				window.location.href = '/';
-			};
+      $scope.cancelSignup = function() {
+        $window.location.href = '/';
+      };
 
-			$scope.createAccount = function () {
-				$http.post(signupUrl, $scope.data, {
-					withCredentials: true
-				})
-					.success(function (res) {
-						$window.location.href = '/';
-					})
-					.error(function (error) {
-                        $scope.authenticationError = error;
-                        console.log("Failed: " , error);
+      $scope.createAccount = function () {
+        $http.post(signupUrl, $scope.data, {
+          withCredentials: true
+        })
+          .success(function (res) {
+            // account creation also logs the user in
+            authService.setCookies(res, $scope.data);
+            $window.location.href = '/';
+          })
+          .error(function (error) {
+            $scope.authenticationError = error;
+            console.log("Failed: " , error);
 
-                        if(error.indexOf("password")>=0){
+            if(error.indexOf("password")>=0){
 
-                        };
+            };
 
-                        if(error.indexOf("email")>=0){
+            if(error.indexOf("email")>=0){
 
-                        };
+            };
 
-					});
-			}
-		}]);
+          });
+      }
+  }
+  ]);
--- WebServer/routes/login.js
+++ WebServer/routes/login.js
@@ -1,36 +1,38 @@
 var express = require('express');
 var router = express.Router();
-
 var netHelpers = just_include('netHelpers');
 
+var login = function login (req, res) {
+  netHelpers.performAjaxRequest('localhost', 5500, '/api/users/login', 'POST', req.body, function (result) {
+    if (result.error) {
+      res.status(result.error.status).send('Unauthorized');
+      return;
+    }
+    req.session.loopbackId = result.id;
+    req.session.userId = result.userId;
+    res.status(200).send(result);
+  });
+};
+
 router.get('/', function (req, res) {
-	res.render('login', { title: 'Login'});
+  res.render('login', { title: 'Login'});
 });
 
-router.post('/', function (req, res) {
-    netHelpers.performAjaxRequest('localhost', 5500, '/api/users/login', 'POST', req.body, function (resultObject) {
-		if (resultObject.error) {
-			res.status(resultObject.error.status).send('Unauthorized');
-			return;
-		}
-		req.session.loopbackId = resultObject.id;
-		req.session.userId = resultObject.userId;
-		res.status(200).send(resultObject);
-	});
-});
+router.post('/', login);
 
 router.post('/signup', function (req, res) {
-	netHelpers.performAjaxRequest('localhost', 5500, '/api/users', 'POST', req.body, function (resultObject) {
-		if (resultObject.error) {
-			res.status(resultObject.error.status).send(resultObject.error.message);
-			return;
-		}
-		res.status(200).send('Success!');
-	})
+  netHelpers.performAjaxRequest('localhost', 5500, '/api/users', 'POST', req.body, function (result) {
+    if (result.error) {
+      res.status(result.error.status).send(result.error.message);
+      return;
+    }
+    // proceed with login (assume we'll have email verification later)
+    login(req, res);
+  })
 });
 
 router.get('/signup', function (req, res) {
-	res.render('signup', { title: 'Signup'});
+  res.render('signup', { title: 'Signup'});
 });
 
 module.exports = router;
