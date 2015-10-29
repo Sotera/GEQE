4109adece55dbac39f52586f025328b9fbf48019
Luke Wendling <luke@lukewendling.com>
2015-10-29 15:40:40 -0500

    bring back toggle arrows and set init panel size to 0

--- WebServer/public/javascripts/app.js
+++ WebServer/public/javascripts/app.js
@@ -1,10 +1,1 @@
 // NOTE: most top-level config happens in layoutController
-
-// manually trigger hiding the left splitbar. timeout is required to accomodate
-//   async setup in angular-ui-layout.
-// TODO: remove when (if) angular-ui-layout supports initial collapsed state.
-setTimeout(hideLeftSplitbar, 500);
-
-function hideLeftSplitbar() {
-  $($('div.ui-splitbar .ui-splitbar-icon-left')[0]).trigger('click');
-}
