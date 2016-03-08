70b47f15a5642bb3e6f17cc36b121497266641d4
drJAGartner <dr.j.a.gartner@gmail.com>
2016-03-07 18:03:56 -0800

    add stream processing

---
+++ geqe-ml/lib/stream_processing.py
@@ -0,0 +1,16 @@
+
+def createBin(record):
+    key = record[0]
+    p1 = key.find('_')
+    p2 = key.find('_', p1+1)
+    str_dt = key[:p1]
+    str_lt = key[p1+1:p2]
+    str_ln = key[p2:]
+    d_ret = {'date':str_dt, 'lat':str_lt, 'lon':str_ln, 'hours':{}}
+    itt = record[1]
+    for row in itt:
+        hour = str(row.dt.hour)
+        if hour in d_ret['hours'].keys():
+            d_ret['hours'][hour].append(row.text)
+        else:
+            d_ret['hours'][hour] = [row.text]
