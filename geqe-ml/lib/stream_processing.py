4e4754bd2d584f8560d31e0ab5012f83ea880d61
drJAGartner <dr.j.a.gartner@gmail.com>
2016-03-09 10:23:56 -0800

    mod to make writing to ES from spark easier

--- geqe-ml/lib/stream_processing.py
+++ geqe-ml/lib/stream_processing.py
@@ -1,3 +1,4 @@
+import json
 
 def create_bin(record):
     key = record[0]
@@ -6,12 +7,13 @@ def create_bin(record):
     str_dt = key[:p1]
     str_lt = key[p1+1:p2]
     str_ln = key[p2+1:]
-    d_ret = {'date':str_dt, 'lat':str_lt, 'lon':str_ln, 'hours':{}}
+    d_ret = {'date':str_dt, 'location': {'type':'point', 'coordinates': (str_lt, str_ln)}, 'hours':{}}
     itt = record[1]
     for row in itt:
         hour = str(row.dt.hour)
         if hour in d_ret['hours'].keys():
-            d_ret['hours'][hour].append(row.text)
+            ind = str(max(d_ret['hours'][hour].keys()) + 1)
+            d_ret['hours'][hour][ind] = row.text
         else:
-            d_ret['hours'][hour] = [row.text]
-    return (key, d_ret)
+            d_ret['hours'][hour]['1'] = row.text
+    return (key, json.dumps(d_ret))
