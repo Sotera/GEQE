8c84aed21325beaf4cf6f21f5ac4056cdfac5a05
drJAGartner <dr.j.a.gartner@gmail.com>
2016-03-11 16:30:42 -0800

    add time filter function

--- geqe-ml/precomputeTrainerPoints.py
+++ geqe-ml/precomputeTrainerPoints.py
@@ -22,15 +22,21 @@ def main():
     sc = SparkContext(conf = conf)
 
     bc_lStopWords = fspLib.load_stopwords(sc,'inputFiles/stopWordList.txt','')
+    dt_low = datetime.date(2015,12,1)
+    bc_low = sc.broadcast(dt_low)
+    dt_high = datetime.date(2016,1,1)
+    bc_high = sc.broadcast(dt_high)
+
     sqlContext = SQLContext(sc)
     sqlContext.registerFunction("hasScorableWord", lambda text: fspLib.hasScorableWord(text,True,bc_lStopWords),returnType=BooleanType())
+    sqlContext.registerFunction("inDateWindow", lambda dt: in_time_window(dt, bc_low, bc_high), returnType=BooleanType())
 
     raw = to_parquet.csvToDataFrame(sc, sqlContext, "...", 66).cache()
     n_raw = raw.count()
     big_print("Read in " + str(n_raw) + " tweets")
 
     raw.registerTempTable("raw")
-    sqlCommand = "SELECT * from raw WHERE hasScorableWord(text)"
+    sqlCommand = "SELECT * from raw WHERE hasScorableWord(text) AND inDateWindwo(dt)"
     df = sqlContext.sql(sqlCommand).cache()
     n_df = df.count()
     big_print("Binning " + str(n_df) + " entries with scorable words")
